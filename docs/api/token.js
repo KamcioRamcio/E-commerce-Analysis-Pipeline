09751638504d72538d5f4e9683bfa02a434e740e
kamcio <kamilecki12@gmail.com>
2025-03-22 11:24:55 +0000

    fix

--- docs/api/token.js
+++ docs/api/token.js
@@ -4,9 +4,13 @@ const METABASE_SITE_URL = process.env.METABASE_SITE_URL || "http://localhost:300
 const METABASE_SECRET_KEY = process.env.METABASE_SECRET_KEY;
 
 export default function handler(req, res) {
-  // Enable CORS
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Origin', 'https://kamcioramcio.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
 
   try {
     const payload = {
