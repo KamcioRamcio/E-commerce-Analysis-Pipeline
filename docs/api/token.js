38c06984587ea0dde019afbd9a5d4f92e178557d
kamcio <kamilecki12@gmail.com>
2025-03-22 11:27:07 +0000

    fix

--- docs/api/token.js
+++ docs/api/token.js
@@ -4,7 +4,7 @@ const METABASE_SITE_URL = process.env.METABASE_SITE_URL || "http://localhost:300
 const METABASE_SECRET_KEY = process.env.METABASE_SECRET_KEY;
 
 export default function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://kamcioramcio.github.io');
+  res.setHeader('Access-Control-Allow-Origin', 'https://kamcioramcio.github.io/E-commerce-Analysis-Pipeline/');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
 
